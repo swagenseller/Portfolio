28e281edd4624cf37ab9117a48260734f7533f52
Jeff <jwagense@ucsc.edu>
2020-05-26 23:49:02 -0700

    created appList method for projects

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 const AppCard = (props) => {
-
-    return (
-        <Card>
-            <Image src={props.item.image} wrapped ui={false}/>
-            <Card.Content>
-                <Card.Header>{props.item.name}</Card.Header>
-                <Card.Description>
-                   {props.item.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                    <Icon name='github'>
-                        Github
-                    </Icon>
-                </a>
-                { props.item.demo.length > 0 &&
-                    <a>
-                        Demo
-                    </a>
-                }
-            </Card.Content>                
-        </Card>
-    );
+	return (
+		<Card>
+			<Image src={props.item.image} wrapped ui={false} />
+			<Card.Content>
+				<Card.Header>{props.item.name}</Card.Header>
+				<Card.Description>{props.item.description}</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<a href={props.item.github}>
+					<Icon name="github">GitHub</Icon>
+				</a>
+				{props.item.demo.length > 0 && <a>Demo</a>}
+			</Card.Content>
+		</Card>
+	);
 };
 
 export default AppCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,68 +1,53 @@
-import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
-import AppCard from './AppCard';
+import React from "react";
+import AppCard from "./AppCard";
 
 const apps = [
-    {
-        image:'',//require('../images/gem.png'), 
-        name: 'SpyMaster Randomizer', 
-        description: 'This is a simple web app that creates a random board for the Spymaster.',
-        demo: '',
-        github: 'https://github.com/swagenseller/CodeNameRandomizer'   
-    },
-    {
-        image: '', 
-        name: 'Pet Food Table', 
-        description: 'An excel like web app for fictional Pet Food inventory.',
-        demo: '',
-        github: 'https://github.com/swagenseller/ExcellyWebApp'     
-    },
-    {
-        image:'', 
-        name: 'Color Game', 
-        description: 'RGB Color guessing game',
-        demo: '',
-        github: 'https://github.com/swagenseller/ColorGame'     
-    },
-]
+	{
+		id: 0,
+		image: "", //require('../images/gem.png'),
+		name: "SpyMaster Randomizer",
+		description:
+			"This is a simple web app that creates a random board for the Spymaster.",
+		demo: "",
+		github: "https://github.com/swagenseller/CodeNameRandomizer",
+	},
+	{
+		id: 1,
+		image: "",
+		name: "Pet Food Table",
+		description: "An excel like web app for fictional Pet Food inventory.",
+		demo: "",
+		github: "https://github.com/swagenseller/ExcellyWebApp",
+	},
+	{
+		id: 2,
+		image: "",
+		name: "Color Game",
+		description: "RGB Color guessing game",
+		demo: "",
+		github: "https://github.com/swagenseller/ColorGame",
+	},
+];
 
 class Projects extends React.Component {
+	appList() {
+		return apps.map((i) => {
+			return (
+				<div key={i.id}>
+					<AppCard item={i} />
+				</div>
+			);
+		});
+	}
 
-    testCard() {
-        return (
-            <Card>
-                <Image src={''} wrapped ui={false}/>
-                <Card.Content>
-                    <Card.Header>SpyMaster Randomizer</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Vanilla Js</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        This is a simple web app that creates a random board for the Spymaster.
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <a>
-                        <Icon name='github'>
-                            Github
-                        </Icon>
-                    </a>
-                </Card.Content>                
-            </Card>
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Projects Page</h1>
-                {/*<div>{this.testCard()}</div>*/}
-                {<AppCard item = {apps[0]} />}
-                {<AppCard item = {apps[1]} />}
-            
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<h1>Projects Page</h1>
+				{this.appList()}
+			</div>
+		);
+	}
 }
 
 export default Projects;
